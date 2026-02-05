d0dc89f32e4dac0b8c328f7e47b45719975da634
hpo1o <brbrbrmuller@gmail.com>
2026-02-05 15:53:44 +0100

    update readme and pool logic

--- README.md
+++ README.md
@@ -64,3 +64,4 @@ $ forge --help
 $ anvil --help
 $ cast --help
 ```
+TEST GITHUB UPDATE
--- src/Pool.sol
+++ src/Pool.sol
@@ -2,11 +2,14 @@
 pragma solidity ^0.8.20;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import { SwapMath } from "./libraries/SwapMath.sol";
+
 
 /// @title Pool — базовый AMM-пул для двух токенов
 /// @notice Безопасный пул для тестов и портфолио, с фиксированной комиссией
-contract Pool {
+contract Pool is ERC20{
     using SafeERC20 for IERC20;
 
     IERC20 public immutable token0;
@@ -15,12 +18,17 @@ contract Pool {
     uint256 private reserve0;
     uint256 private reserve1;
 
+    uint256 public price0CumulativeLast;
+    uint256 public price1CumulativeLast;
+    uint32  public blockTimestampLast;
+
+
     uint256 public constant FEE_BPS = 30; // 0.3%
     uint256 public constant BPS_DENOM = 10_000;
 
     event Swap(address indexed sender, address tokenIn, uint256 amountIn, uint256 amountOut, address indexed to);
 
-    constructor(address _token0, address _token1) {
+    constructor(address _token0, address _token1) ERC20("LP TOKEN", "LP") {
         require(_token0 != _token1, "IDENTICAL_ADDRESSES");
         token0 = IERC20(_token0);
         token1 = IERC20(_token1);
@@ -30,6 +38,39 @@ contract Pool {
     function getReserves() external view returns (uint256, uint256) {
         return (reserve0, reserve1);
     }
+
+    function _updateTWAP() internal {
+        uint32 blockTimestamp = uint32(block.timestamp);
+        uint32 timeElapsed = blockTimestamp - blockTimestampLast;
+
+        if (timeElapsed > 0 && reserve0 > 0 && reserve1 > 0) {
+            // price0 = reserve1 / reserve0
+            price0CumulativeLast += (reserve1 * 1e18 / reserve0) * timeElapsed;
+            // price1 = reserve0 / reserve1
+            price1CumulativeLast += (reserve0 * 1e18 / reserve1) * timeElapsed;
+        }
+
+        blockTimestampLast = blockTimestamp;
+    }
+
+    function getTWAP() external view returns (uint256 price0TWAP) {
+        require(blockTimestampLast != 0, "NO_TWAP_DATA");
+
+        uint32 timeElapsed = uint32(block.timestamp) - blockTimestampLast;
+        require(timeElapsed > 0, "NO_TIME_ELAPSED");
+
+        uint256 cumulative = price0CumulativeLast;
+
+        if (reserve0 > 0 && reserve1 > 0) {
+            uint256 currentPrice = (reserve1 * 1e18) / reserve0;
+            cumulative += currentPrice * timeElapsed;
+        }
+
+        price0TWAP = cumulative / timeElapsed;
+    }
+
+
+
 
     /// @notice Выполняет свап tokenIn -> tokenOut
     /// @param tokenIn токен, который пользователь отдаёт
@@ -41,6 +82,7 @@ contract Pool {
         external
         returns (uint256 amountOut)
     {
+        _updateTWAP();
         bool zeroForOne = tokenIn == address(token0);
         (uint256 reserveIn, uint256 reserveOut) = zeroForOne ? (reserve0, reserve1) : (reserve1, reserve0);
 
@@ -86,12 +128,53 @@ contract Pool {
         amountOut = (amountInWithFee * reserveOut) / (reserveIn + amountInWithFee);
     }
 
-    /// @notice Функция для добавления ликвидности (для тестов)
-    function addLiquidity(uint256 amount0, uint256 amount1) external {
-        require(amount0 > 0 && amount1 > 0, "ZERO_AMOUNT");
+    function addLiquidity(uint256 amount0, uint256 amount1)
+        external
+        returns (uint256 liquidity)
+    {
+        _updateTWAP();
         token0.safeTransferFrom(msg.sender, address(this), amount0);
         token1.safeTransferFrom(msg.sender, address(this), amount1);
+
+        if (totalSupply() == 0) {
+            liquidity = SwapMath.sqrt(amount0 * amount1);
+        } else {
+            liquidity = SwapMath.min(
+                (amount0 * totalSupply()) / reserve0,
+                (amount1 * totalSupply()) / reserve1
+            );
+        }
+
+        require(liquidity > 0, "INSUFFICIENT_LIQUIDITY_MINTED");
+
+        _mint(msg.sender, liquidity);
+
         reserve0 += amount0;
         reserve1 += amount1;
     }
+
+    function removeLiquidity(uint256 liquidity)
+        external 
+        returns (uint256 amount0, uint256 amount1)
+    {
+        _updateTWAP();
+        require(liquidity > 0, "ZERO_LIQUIDITY");
+
+        uint256 supply = totalSupply();
+
+        amount0 = (liquidity * reserve0) / supply;
+        amount1 = (liquidity * reserve1) / supply;
+
+        require(amount0 > 0 && amount1 > 0, "INSUFFICIENT_OUTPUT");
+
+        _burn(msg.sender, liquidity);
+
+        reserve0 -= amount0;
+        reserve1 -= amount1;
+
+        token0.safeTransfer(msg.sender, amount0);
+        token1.safeTransfer(msg.sender, amount1);
+    }
+
+
 }
